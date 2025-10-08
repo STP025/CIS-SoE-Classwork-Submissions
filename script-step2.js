93e9eecfb3200cb46b880474e856267a88586a4b
Sandip Magar <thapasandip080@gmail.com>
2025-10-08 11:01:44 +0545

    Inital Commit

---
+++ script-step2.js
@@ -0,0 +1,9 @@
+document.getElementById('submitBtn').addEventListener('click', function() {
+    let input = document.getElementById('inputField').value;
+    document.getElementById('output').innerText = "You entered: " + input;
+});
+
+
+
+
+
